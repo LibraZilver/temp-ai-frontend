97f17ccf7b46a62fd6477b62689145fe8ed37f78
LaterNever <watchara.che@gmail.com>
2025-07-16 14:20:06 +0700

    change react-router-dom react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { useState, useLayoutEffect } from 'react'
+import { Router, Routes, Route } from 'react-router'
+import { createBrowserHistory } from 'history'
 import Home from './components/Home'
 import Login from './components/Login'
 import './App.css'
 
+const history = createBrowserHistory()
+
 function App(): React.JSX.Element {
+  const [state, setState] = useState({
+    action: history.action,
+    location: history.location,
+  })
+
+  useLayoutEffect(() => history.listen(setState), [])
+
   return (
-    <Router>
+    <Router location={state.location} navigator={history}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
